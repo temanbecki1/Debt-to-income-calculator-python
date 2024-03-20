9c41123587fca933f93d7d512f8115901011a048
Teman Beck <13569856-temanbecki1@users.noreply.replit.com>
2024-03-20 03:22:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,128 @@
+def main():
+  ## This variable will contain a list of expenses
+  expenses = []
+  ## This variable will store the result of a function get_mortgage()
+  mortgage = get_mortgage()
+  expenses.append(mortgage)
+
+  ## This variable will store the result of a function get_student_loans()
+  student_loans = get_student_loans()
+  expenses.append(student_loans)
+
+  ## This variable will store the result of a function get_personal_loans()
+  personal_loans = get_personal_loans()
+  expenses.append(personal_loans)
+
+  ## This variable will store the result of a function get_car_loan()
+  car_loan = get_car_loan()
+  expenses.append(car_loan)
+
+  ## This variable will store the result of a function get_credit_cards()
+  credit_card_payments = get_credit_cards()
+  expenses.append(credit_card_payments)
+
+  ## This variable will store the result of a function get_other_recurring()
+  other_recurring = get_other_recurring()
+  expenses.append(other_recurring)
+
+  ## This variable will store the result of a function get_gross_income()
+  gross_income = get_gross_income()
+
+  ## This variable will store the result of a function get_total_payments() that will return the total payments.
+  monthly_total = get_total_payments(expenses)
+
+  ## This variable will store the result of a function get_dti() that will return the debt to income percentage.
+  debt_to_income = get_dti(monthly_total, gross_income)
+
+  ## This will print the debt_to_income to the screen
+  print('Your debt to income ratio is ' + str(debt_to_income) + '%')
+
+def get_mortgage():
+  mortgage = 0.00
+
+  while True:
+      mortgage = input('Enter your monthly mortgage: ')
+      is_number = mortgage.isnumeric() 
+      if is_number:
+          mortgage = float(mortgage)
+          if mortgage >= 0:
+            return mortgage
+
+def get_student_loans():
+  student_loan = 0.00
+
+  while True:
+      student_loan = input('Enter your monthly student loan payment: ')
+      is_number = student_loan.isnumeric()
+      if is_number:
+        student_loan = float(student_loan)
+        if student_loan >= 0:
+          return student_loan
+
+def get_personal_loans():
+  personal_loan = 0.00
+
+  while True:
+      personal_loan = input('Enter your monthly personal loan payment: ')
+      is_number = personal_loan.isnumeric()
+      if is_number:
+        personal_loan = float(personal_loan)
+        if personal_loan >= 0:
+          return personal_loan
+
+def get_car_loan():
+  car_loan = 0.00
+
+  while True:
+      car_loan = input('Enter your monthly car payment: ')
+      is_number = car_loan.isnumeric()
+      if is_number:
+        car_loan = float(car_loan)
+        if car_loan >= 0:
+          return car_loan
+
+def get_credit_cards():
+  credit_cards = 0.00
+
+  while True:
+      credit_cards = input('Enter your monthly credit card payment: ')
+      is_number = credit_cards.isnumeric()
+      if is_number:
+        credit_cards = float(credit_cards)
+        if credit_cards >= 0:
+          return credit_cards
+
+def get_other_recurring():
+  other_recurring = 0.00
+
+  while True:
+      other_recurring = input(
+          'Enter your monthly recurring expenses not in any of the other categories.: ')
+      is_number = other_recurring.isnumeric()
+      if is_number:
+        other_recurring = float(other_recurring)
+        if other_recurring >= 0:
+          return other_recurring
+
+def get_gross_income():
+  gross_income = 0.00
+
+  while True:
+      gross_income = input('Enter your monthly gross income: ')
+      is_number = gross_income.isnumeric()
+      if is_number:
+        gross_income = float(gross_income)
+        if gross_income >= 0:
+          return gross_income
+
+def get_total_payments(expenses):
+  total_payments = sum(expenses)
+  return total_payments
+
+
+def get_dti(monthly_total, gross_income):
+  dti = (monthly_total / gross_income) * 100
+
+  return dti
+
+main()
